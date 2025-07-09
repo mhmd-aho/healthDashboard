de2144079c6f126df9554317a5b51e66a154f595
mhmd-aho <mhmdabouhamoud@gmail.com>
2025-07-09 19:40:37 +0300

    fixed base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/task-manager/',
+  base: '/healthDashboard/',
   plugins: [react(),
         tailwindcss(),
   ],
